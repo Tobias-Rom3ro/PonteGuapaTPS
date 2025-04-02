848852976bab158182b94cc9b6a3e10e31ec34de
Tobias-Rom3ro <taromero@unimagdanela.edu.co>
2025-04-02 10:43:38 -0500

    Add comment for password visibility toggle functionality

--- view/scripts/PasswordVisibilty.js
+++ view/scripts/PasswordVisibilty.js
@@ -1,3 +1,4 @@
+// Cambio de la visibilidad de la contraseña
 document.querySelector('.eye-icon').addEventListener('click', function() {
     const passwordInput = document.getElementById('password');
     if (passwordInput.type === 'password') {
