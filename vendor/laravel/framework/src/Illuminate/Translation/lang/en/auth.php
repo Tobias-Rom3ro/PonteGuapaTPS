c4271f5d2034ef15b8267f7796984a0fe83ad23b
Tobias-Rom3ro <taromero@unimagdanela.edu.co>
2025-04-09 08:42:23 -0500

    Adding files

---
+++ vendor/laravel/framework/src/Illuminate/Translation/lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Los credenciales proporcionadas no existen en nuestro registro.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Muchos intentos, por favor intenta de nuevo en: seconds segundos.',
+
+];
